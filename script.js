6813d530809d49e06426bfe5332b5bd97a701bd7
Pedro Henrique <p.martims120@gmail.com>
2023-06-28 13:15:53 -0300

    requisito 21 incompleto

--- script.js
+++ script.js
@@ -34,3 +34,56 @@ textarea.addEventListener('keyup', (event) => {
   contador -= textLenf;
   counter.innerText = `${contador}`;
 });
+
+function getFullName() {
+  const nome = document.getElementById('input-name').value;
+  const sobrenome = document.getElementById('input-lastname').value;
+  return `${nome} ${sobrenome}`;
+}
+
+function getEmail() {
+  return document.getElementById('input-email').value;
+}
+
+function getHouse() {
+  return document.getElementById('house').value;
+}
+
+function getFamily() {
+  return document.getElementById('label-family').value;
+}
+
+function getContente() {
+  return document.getElementById('label-content').value;
+}
+
+function getRate() {
+  return document.getElementById('label-rate').value;
+}
+
+function getText() {
+  return document.getElementById('textarea').value;
+}
+
+function getInfos() {
+  const fullName = getFullName();
+  const mail = getEmail();
+  const house = getHouse();
+  const family = getFamily();
+  const content = getContente();
+  const rate = getRate();
+  const text = getText();
+
+  const formData = {
+    Nome: fullName,
+    Email: mail,
+    Casa: house,
+    Família: family,
+    Matérias: content,
+    Avaliação: rate,
+    Observações: text,
+  };
+  return formData;
+}
+const formData = getInfos();
+localStorage.setItem('form-Data', JSON.stringify(formData()));
